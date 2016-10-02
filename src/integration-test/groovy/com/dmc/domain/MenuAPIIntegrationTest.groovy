354d8a2abeac4c6db34e879751a66ff890f3a927
Diego Correa <diegomartincorrea@gmail.com>
2016-10-01 22:58:18 -0300

    Working with Integration test

--- src/integration-test/groovy/com/dmc/domain/MenuAPIIntegrationTest.groovy
+++ src/integration-test/groovy/com/dmc/domain/MenuAPIIntegrationTest.groovy
@@ -1,8 +1,13 @@
 package com.dmc.domain
 
+import grails.converters.JSON
 import grails.test.mixin.Mock
 import grails.test.mixin.integration.Integration
 import grails.transaction.Rollback
+import grails.web.mime.MimeType
+import groovyx.net.http.RESTClient
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -18,7 +23,36 @@ class MenuAPIIntegrationTest extends Specification {
     @Shared
     def grailsApplication
 
+    @Value('${local.server.port}')
+    Integer serverPort
 
+    RESTClient rest
 
+    def setup() {
+        assert Menu.count == 0
+        assert Item.count == 0
+
+        rest = new RESTClient("http://localhost:${serverPort}${grailsApplication.config['server.contextPath']}/")
+    }
+
+    def cleanup() {
+        Menu.where { id != null }.deleteAll()
+        Item.where { id != null }.deleteAll()
+    }
+
+    void "test can request menu list"() {
+        given:
+        def menu = new Menu(description: "Milanesa Napo").save(failOnError: true)
+
+        when:
+        def response = rest.get(path: "api/menus")
+
+        then:
+        with(response) {
+            status == HttpStatus.OK.value()
+            contentType == MimeType.JSON.name
+            responseData == ([menu] as JSON)
+        }
+    }
 
 }
